84a0329bcf9581b16828207daf1e95f38a1d1355
Mina Her <minacle@live.com>
2022-06-17 13:29:11 +0900

    Add Mabinogi Classic fonts

---
+++ Casks/font-mabinogi-classic-otf.rb
@@ -0,0 +1,10 @@
+cask "font-mabinogi-classic-otf" do
+
+    desc "The typeface that captures the emotions of Mabinogi, the fantasy life."
+    homepage "https://mabinogi.nexon.com/page/pds/font.asp"
+    name "Mabinogi Classic OTF"
+    sha256 :no_check
+    version :latest
+    url "https://mabinogi.vod.nexoncdn.co.kr/font/Mabinogi_Classic_OTF.otf"
+    font "Mabinogi_Classic_OTF.otf"
+end
---
+++ Casks/font-mabinogi-classic-ttf.rb
@@ -0,0 +1,10 @@
+cask "font-mabinogi-classic-ttf" do
+
+    desc "The typeface that captures the emotions of Mabinogi, the fantasy life."
+    homepage "https://mabinogi.nexon.com/page/pds/font.asp"
+    name "Mabinogi Classic TTF"
+    sha256 :no_check
+    version :latest
+    url "https://mabinogi.vod.nexoncdn.co.kr/font/Mabinogi_Classic_TTF.ttf"
+    font "Mabinogi_Classic_TTF.ttf"
+end
