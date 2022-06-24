47aa93c659c3cd18b547c2355a113152edad214d
Mina Her <minacle@live.com>
2022-06-24 18:31:40 +0900

    Add Maplestory fonts

---
+++ Casks/font-maplestory-otf.rb
@@ -0,0 +1,11 @@
+cask "font-maplestory-otf" do
+
+    desc "It was created by reflecting MapleStory's unique cute and lively characters and fun and enjoyable game atmosphere."
+    homepage "https://maplestory.nexon.com/Media/Font"
+    name "MaplestoryFont OTF"
+    sha256 :no_check
+    version :latest
+    url "https://ssl.nexon.com/s2/Game/Maplestory/Maple2013/image/data/MaplestoryFont_OTF.zip"
+    font "Maplestory OTF Bold.otf"
+    font "Maplestory OTF Light.otf"
+end
---
+++ Casks/font-maplestory-ttf.rb
@@ -0,0 +1,11 @@
+cask "font-maplestory-ttf" do
+
+    desc "It was created by reflecting MapleStory's unique cute and lively characters and fun and enjoyable game atmosphere."
+    homepage "https://maplestory.nexon.com/Media/Font"
+    name "MaplestoryFont TTF"
+    sha256 :no_check
+    version :latest
+    url "https://ssl.nexon.com/s2/Game/Maplestory/Maple2013/image/data/MaplestoryFont_TTF.zip"
+    font "Maplestory Bold.ttf"
+    font "Maplestory Light.ttf"
+end
