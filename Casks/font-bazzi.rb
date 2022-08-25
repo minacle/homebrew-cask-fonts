a3a3ea8d0a0c4b2f716cc3289eeafecd94630745
Mina Her <minacle@live.com>
2022-08-25 11:26:34 +0900

    Add Nexon Bazzi fonts

---
+++ Casks/font-bazzi.rb
@@ -0,0 +1,11 @@
+cask "font-bazzi" do
+
+    desc "Reminiscent of the round and cute shape of Bazzi, we focused on not missing out on its function as a typeface, and it was produced taking advantage of the original design of the award-winning work."
+    homepage "http://levelup.nexon.com/font/index.aspx?page=3"
+    name "Nexon Bazzi Font"
+    sha256 :no_check
+    version :latest
+    url "http://s.nexon.com/s2/levelup/fontSite/files/BazziFont/A_BAZZI_Font.zip"
+    font "Bazzi OTF.otf"
+    font "Bazzi.ttf"
+end
