e72124938de39df854c8e69d3a6ae1e82bfc90ff
Mina Her <minacle@live.com>
2022-08-25 11:05:15 +0900

    Add CookieRun fonts

---
+++ Casks/font-cookierun-otf.rb
@@ -0,0 +1,12 @@
+cask "font-cookierun-otf" do
+
+    desc "The unique identity of Cookie Run, which is cute and challenging, is included in the font."
+    homepage "https://www.cookierunfont.com"
+    name "CookieRun Typeface OTF"
+    sha256 :no_check
+    version :latest
+    url "https://www.cookierunfont.com/static/download/CookieRunFont_OTF.zip"
+    font "CookieRun Black.otf"
+    font "CookieRun Bold.otf"
+    font "CookieRun Regular.otf"
+end
---
+++ Casks/font-cookierun-ttf.rb
@@ -0,0 +1,12 @@
+cask "font-cookierun-ttf" do
+
+    desc "The unique identity of Cookie Run, which is cute and challenging, is included in the font."
+    homepage "https://www.cookierunfont.com"
+    name "CookieRun Typeface TTF"
+    sha256 :no_check
+    version :latest
+    url "https://www.cookierunfont.com/static/download/CookieRunFont_TTF.zip"
+    font "CookieRun Black.ttf"
+    font "CookieRun Bold.ttf"
+    font "CookieRun Regular.ttf"
+end
